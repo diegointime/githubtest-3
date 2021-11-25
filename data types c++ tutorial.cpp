e7dfa6cf43b60a2027cc77b15ea0724e3b5e3e00
diegointime <95004875+diegointime@users.noreply.github.com>
2021-11-25 00:34:34 -0300

    quick change
    i´m trying to test something

---
+++ data types c++ tutorial.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char grade = 'A'; /*single characters*/
+	string phrase = "giraffe academy"; /*plain text*/
+	int age = 50; /*whole numbers*/
+	double gpa = 2.3; /* decimal numbers*/ 
+    bool isMale = true; /*true or false*/
+    
+    
+
+
+
+
+
+
+    return 0;
+}
